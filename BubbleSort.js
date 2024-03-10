d4eb7f30740f3bea7a635a3200a87d99722e4c85
Dema <80632445+DemaPy@users.noreply.github.com>
2024-03-10 19:10:01 +0100

    Create BubbleSort.js

---
+++ BubbleSort.js
@@ -0,0 +1,26 @@
+const swap = (arr, first, second) => [arr[first], arr[second]] = [arr[second], arr[first]]
+
+const compare = (first, second) => {
+    return first - second
+}
+
+const bubble_sort  = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (compare(arr[j], arr[j+1]) > 0) {
+                swap(arr, j, j+1)
+            }
+        }
+    }
+}
+
+
+const bubble_sort  = (arr) => {
+    for (let i = arr.length - 1; i > 0; i--) {
+        for (let j = 0; j < i; j++) {
+            if (compare(arr[j], arr[j+1]) > 0) {
+                swap(arr, j, j+1)
+            }
+        }
+    }
+}
