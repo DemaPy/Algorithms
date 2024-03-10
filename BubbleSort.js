59dc494b7919b06c7c64e17c57fd790440e8246c
Dema <80632445+DemaPy@users.noreply.github.com>
2024-03-10 22:55:00 +0100

    Update BubbleSort.js

--- BubbleSort.js
+++ BubbleSort.js
@@ -16,8 +16,8 @@ const bubble_sort  = (arr) => {
 
 
 const bubble_sort  = (arr) => {
-    for (let i = arr.length - 1; i > 0; i--) {
-        for (let j = 0; j < i; j++) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
             if (compare(arr[j], arr[j+1]) > 0) {
                 swap(arr, j, j+1)
             }
